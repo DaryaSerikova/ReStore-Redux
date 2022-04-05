de69f46407d8907fb35aa07a2181d9722ce04dd9
Darya Serikova <dvserikova@gmail.com>
2022-04-05 18:29:21 +0700

    add styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import ShopHeader from "../shop-header/shop-header.js";
 import { HomePage, CartPage } from '../pages/index.js'
 import './app.css';
 
@@ -7,17 +8,19 @@ import './app.css';
 
 const App = () => {
   return(
-    <Routes>
-      <Route 
-        path='/'
-        element={<HomePage/>}
-      />
-      <Route 
-        path='/cart'
-        element={<CartPage/>}
-      />
-    </Routes>
-    // <div>App</div>
+    <main role="main" className="container">
+      <ShopHeader numItems={5} total={210}/>
+      <Routes>
+        <Route 
+          path='/'
+          element={<HomePage/>}
+        />
+        <Route 
+          path='/cart'
+          element={<CartPage/>}
+        />
+      </Routes>
+    </main>
   );
 };
 
--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import BookList from '../book-list';
+import ShoppingCartTable from '../shopping-cart-table/shopping-cart-table';
 
 const HomePage = () => {
 
   return (
     // <div>Home Page</div>
-    <BookList />
+    <div>
+      <BookList />
+      <ShoppingCartTable />
+    </div>
+
   );
 };
 
