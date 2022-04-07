1f7c803a918cd81ed2df50edbec5c1d119325758
Darya Serikova <dvserikova@gmail.com>
2022-04-07 19:08:16 +0700

    Middleware

--- src/store.js
+++ src/store.js
@@ -1,5 +1,29 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
+
+
+//MIDDLEWARE 
+//Мы не получаем полный доступ к store. Доступ только к двум ф-циям:
+// { getState, dispatch }
+// const logMiddleware = (store) => (dispatch) => (action) => { // часто dispatch это next (в документации)
+//   console.log(action.type);
+//   return dispatch(action);
+// };
+
+// const stringMiddleware = (store) => (dispatch) => (action) => {
+//     if (typeof action === 'string') {
+//     return dispatch({
+//       type: action
+//     });
+//   }
+
+//   return dispatch(action);
+// };
+
+// const store = createStore(reducer, applyMiddleware(
+//   stringMiddleware, logMiddleware)); // порядок важен
+
+// ---------------------------------------------------------
 
 // STORE ENHANCER
 // const enhancer = (createStore) => (...args) => {
@@ -32,7 +56,9 @@ import reducer from './reducers';
 //   return originalDispatch(action);
 // };
 
-// store.dispatch('HELLO');
+// store.dispatch('HELLO'); // это тестер
+
+
 
 const store = createStore(reducer);
 
