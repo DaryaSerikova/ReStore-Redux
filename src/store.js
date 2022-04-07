b3c6d4c209f08319eb9d4ea2c57d433cada5ee0a
Darya Serikova <dvserikova@gmail.com>
2022-04-07 18:45:49 +0700

    Store Enhancer and Monkey Patching

--- src/store.js
+++ src/store.js
@@ -1,7 +1,38 @@
 import { createStore } from 'redux';
-
 import reducer from './reducers';
 
+// STORE ENHANCER
+// const enhancer = (createStore) => (...args) => {
+//   const store = createStore(...args);
+//   const originalDispatch = store.dispatch;
+//   store.dispatch = (action) => {
+//     if (typeof action === 'string') {
+//       return originalDispatch({
+//         type: action
+//       });
+//     }
+
+//     return originalDispatch(action);
+//   };
+
+//   return store;
+// };
+
+// const store = createStore(reducer, enhancer);
+
+// MONKEY PATCHING //ИСПРАВЛЕНИЕ ОБЕЗЬЯНЫ
+// const originalDispatch = store.dispatch;
+// store.dispatch = (action) => {
+//   if (typeof action === 'string') {
+//     return originalDispatch({
+//       type: action
+//     });
+//   }
+
+//   return originalDispatch(action);
+// };
+
+// store.dispatch('HELLO');
 
 const store = createStore(reducer);
 
